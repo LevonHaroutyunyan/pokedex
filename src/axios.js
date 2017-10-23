05e5bde172757f8f408d5a1e1d046028baa84fef
Levon Haroutyunyan <levon.haroutyunyan@gmail.com>
2017-10-23 15:35:10 +0400

    first

---
+++ src/axios.js
@@ -0,0 +1,26 @@
+// import base from 'axios';
+// const axios = base.create({
+//   baseURL: 'https://cors.now.sh/https://pokeapi.co/api/v2/',
+//   // headers: {
+//   //   'Accept': 'application/json',
+//   //   // 'Content-Type': 'application/json',
+//   //   // "Access-Control-Allow-Headers": "Content-Type",
+//   //   // 'Access-Control-Request-Headers': "Content-Type",
+//   //   // // 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+//   //   // // 'Origin': 'https://pokeapi.co',
+//   //   // // "Access-Control-Allow-Credentials": "true",
+//   //   // 'Refer': 'https://pokeapi.co',
+//   //   // "Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
+//   //   // "Access-Control-Allow-Origin": "https://pokeapi.co"
+//   // }
+// });
+
+// // axios.interceptors.request.use(config => {
+// //   const newconf = config;
+// //   newconf.headers.ContentType = "application/json";
+// //   return newconf;
+// // }, error => {
+// //   return Promise.reject(error);
+// // });
+// export default axios;
+
